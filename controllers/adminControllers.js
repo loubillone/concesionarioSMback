a03c4410d01a3e6b20ff6e546ed5b3a1f49cc594
Lourdes Billone <lou.billone@gmail.com>
2024-07-30 19:52:08 -0300

    peticion eliminar usuario terminada

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -13,15 +13,29 @@ const listaUsuarios = async (req, res) => {
   }
 };
 
-// const eliminarUsuario = async (req, res) => {
-//   try {
-//     const usuarioEliminar = await Usuarios.findById(req.params.id);
-//     console.log(usuarioEliminar);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+const eliminarUsuario = async (req, res) => {
+  try {
+    const usuarioEliminar = await Usuarios.findById(req.params.id);
+
+    if (!usuarioEliminar) {
+      return res.status(400).json({
+        msg: "El usuario a eliminar no existe",
+      });
+    }
+
+    await Usuarios.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      msg: "Usuario eliminado",
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Por favor contactarse con el administrador",
+    });
+  }
+};
 
 module.exports = {
   listaUsuarios,
+  eliminarUsuario,
 };
--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { listaUsuarios } = require("../controllers/adminControllers");
+const {
+  listaUsuarios,
+  eliminarUsuario,
+} = require("../controllers/adminControllers");
 
 const routerAdmin = express.Router();
 
 routerAdmin.get("/listaUsuarios", listaUsuarios);
-// routerAdmin.delete("/eliminarUsuario:id", eliminarUsuario);
+routerAdmin.delete("/eliminarUsuario/:id", eliminarUsuario);
 
 module.exports = routerAdmin;
