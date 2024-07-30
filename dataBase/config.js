9c1eed07cb54f5a4c0b1704ff1101962929b4a68
Lourdes Billone <lou.billone@gmail.com>
2024-07-29 21:29:44 -0300

    crear usuario terminado

---
+++ dataBase/config.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN);
+
+    console.log("db conectada");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = dbConnection;
